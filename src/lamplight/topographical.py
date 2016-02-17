8bd448d09a506c11f80f9f6c86d456cab1ec7068
probinso <pmoss.robinson@gmail.com>
2016-02-16 21:57:08 -0800

    fixed intractable selection of points, not quite clear enough yet

--- src/lamplight/topographical.py
+++ src/lamplight/topographical.py
@@ -9,7 +9,17 @@ from lamplight import image_info, save_images, image_split
 from lamplight import step_range_gen
 from lamplight import topograph_image, get_index_of, make_clusters
 
+from collections import defaultdict
+
 import matplotlib.pyplot as plt
+import numpy as np # i should never have to do this
+
+
+def take(collection, num):
+    for i, elm in enumerate(collection):
+        if i >= num:
+            break
+        yield elm
 
 
 def interface(filename, directory):
@@ -17,13 +27,19 @@ def interface(filename, directory):
 
     step_gen = step_range_gen()
 
-    r, g, b = image_split(src_image)
-    top_image = topograph_image(g, step_gen)
+    top_image = topograph_image(src_image, step_gen)
+    r, g, b = image_split(top_image)
 
     points_dict = get_index_of(top_image)
-    l_channel, l_intensity = 1, next(step_gen.range) # green at highest intensity
-    clusters = make_clusters(points_dict[l_channel][l_intensity])
 
+    cluster_dict = defaultdict(dict)
+    for l_intensity in take(step_gen.range, 2):
+        for l_channel in points_dict:
+            cluster_dict[l_channel][l_intensity] = make_clusters(points_dict[l_channel][l_intensity])
+
+
+    l_channel, l_intensity = 0, next(step_gen.range)
+    clusters = cluster_dict[l_channel][l_intensity]
     colors = plt.cm.Spectral(np.linspace(0 , 1, len(clusters)))*255
     for i, c in enumerate(clusters):
         for [x , y] in clusters[c]:
