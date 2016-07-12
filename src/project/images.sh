d193b49a43679b73b6b7d6783b3850bb7ff469c0
probinso <pmoss.robinson@gmail.com>
2016-07-12 00:32:54 -0700

    test images

---
+++ src/project/images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in 1 2 3 4;
+do
+    ./lamp-runner.py render input_$i.png .;
+    mv output.png output_$i.png;
+    convert input_$i.png output_$i.png +append $i.png;
+    rm output_$i.png;
+done;
